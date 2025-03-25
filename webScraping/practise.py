54808968c8424df39c19174ce59f8ed0ca09d086
popo3454545 <103442763+popo3454545@users.noreply.github.com>
2025-03-25 23:32:44 +0900

    update 2023/03/25

--- webScraping/practise.py
+++ webScraping/practise.py
@@ -3,9 +3,9 @@ import re
 import os
 from multiprocessing.dummy import Pool
 
-url='http://www.kanunu8.com/book3/6879/'
+start_url='http://www.kanunu8.com/book3/6879/'
 
-def get_resource(url):
+def get_resource(html):
     """
     get resource form web
     url:resource location
@@ -15,6 +15,26 @@ def get_resource(url):
     return html.content.decode('gbk')
 
 print(get_resource(url))
+
+
+def get_toc(html):
+      
+ toc_url_list=[]
+ toc_block=re.findall('正文(.*?)</tbody>',html,re.S)
+ toc_url=re.findall('href="(.*?)"',toc_block,re.S)
+ for  url in toc_url:
+     toc_url_list.append(start_url+url)
+     print(toc_url_list)
+ return toc_url_list
+
+
+def get_article(html):
+
+
+
+
+    
+    return 
 
 
 
