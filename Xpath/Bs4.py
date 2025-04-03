cf63099b4c46b64d13e2e381e7dd34999379f09c
popo3454545 <103442763+popo3454545@users.noreply.github.com>
2025-04-03 23:44:55 +0900

    update

--- Xpath/Bs4.py
+++ Xpath/Bs4.py
@@ -4,10 +4,13 @@ import requests
 
 html=requests.get('http://exercise.kingname.info/exercise_bs_1.html').content.decode()
 soup=BeautifulSoup(html, "lxml")
-info=soup.find(class_='test')
-print(type(info))
-print(info)
-
+# info=soup.find(class_='test')
+# print(type(info))
+# print(info)
+info=soup.find(class_='useful')
+info2=info.find_all('li')
+for result in info2:
+    print(result.string)
 
 
 
