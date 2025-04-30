a16d019292c16ca03185d07a04a76cedd47a7046
popo3454545 <103442763+popo3454545@users.noreply.github.com>
2025-04-30 20:58:21 +0900

    add

---
+++ Study2/graphy.py
@@ -0,0 +1,9 @@
+from  gturtle import *
+makeTurtle("gray")
+width=5
+
+hideTurtle()
+repeat 50:
+ forward(width)
+ right(90)
+ width=width+10
---
+++ Study2/grapy2.py
@@ -0,0 +1,5 @@
+from  gturtle import *
+makeTurtle("gray")
+repeat 3:
+    forward(200)
+    right(120)
