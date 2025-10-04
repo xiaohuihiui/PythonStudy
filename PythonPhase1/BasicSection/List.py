5b11a6b83b86d5d9ba4210510b9081f0b5d659f1
Administrator <976@simple.com>
2025-10-04 21:44:57 +0900

    2025/10/02 english japanese python

---
+++ PythonPhase1/BasicSection/List.py
@@ -0,0 +1,12 @@
+list=["a","b","c","d"]
+print(list)##insert value
+list.insert(0,"aa")
+print(list)
+list.append("bb")##insert value by end
+print(list)
+list[1]="cc" #change value
+print(list)
+list.remove('cc')#delete list value
+print(list)
+list.pop(1)#delete list value
+print(list)
