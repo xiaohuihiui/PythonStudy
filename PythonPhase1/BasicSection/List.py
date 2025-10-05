d0f30aaac05c0d0440d408c5360a943fa05075ad
Administrator <976@simple.com>
2025-10-05 23:47:11 +0900

    2025/10/05 english japanese python

--- PythonPhase1/BasicSection/List.py
+++ PythonPhase1/BasicSection/List.py
@@ -1,12 +1,40 @@
+from xmlrpc.client import escape
+
 list=["a","b","c","d"]
-print(list)##insert value
-list.insert(0,"aa")
-print(list)
-list.append("bb")##insert value by end
-print(list)
-list[1]="cc" #change value
-print(list)
-list.remove('cc')#delete list value
-print(list)
-list.pop(1)#delete list value
-print(list)
+list2=["e","f","g","h"]
+# print(list)##insert value
+# list.insert(0,"aa")
+# print(list)
+# list.append("bb")##insert value by end
+# print(list)
+# list[1]="cc" #change value
+# print(list)
+# list.remove('cc')#delete list value
+# print(list)
+# list.pop(1)#delete list value
+# print(list)
+# list.reverse()
+# print(list)
+# b=list.copy()
+# print(b)
+# print(list+list2)
+# print(list*2)
+#--------------------------------------------------------------------------------
+# list escape
+# print(list[0:13])
+# print(list[0:])
+# print(list[:])
+# print(list[::-1])
+# print(list[::2])
+# print(list[1::2])
+# print(list[-1::2])
+# list3=[]
+# for item in range(10):
+#     list3.append(item*2)
+# print(list3)
+
+# list4=[i**2 for i in range(10)]
+# print(list4)
+
+for index,item in enumerate(list):
+    print(index,item)
